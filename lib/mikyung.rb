97f5c982f924a6bb8bab2ff90d2ea4638e68cb1e
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-15 10:00:27 -0300

    instantiates only if required

--- lib/mikyung.rb
+++ lib/mikyung.rb
@@ -26,7 +26,7 @@ class RelationDrivenObjective
     step = internals.find do |k|
       k.first.matches?(resource)
     end
-    step ? step.last : nil
+    step ? step.last : nil    
   end
 
   private
@@ -36,7 +36,6 @@ class RelationDrivenObjective
   end
 
 end
-
 
 
 # iterates following a series of steps provided a goal and a starting uri.
@@ -52,14 +51,15 @@ class Mikyung
   # keeps changing from a steady state to another until its goal has been achieved
   def run(uri)
     
-    current = Restfulie.at(uri).get!
+    current = Restfulie.at(uri).get
     Restfulie::Common::Logger.logger.debug current.response.body
     
     while(!@goal.completed?(current))
       step = @goal.next_step(current)
       raise "No step was found for #{current} with links #{current.links}" unless step
       Restfulie::Common::Logger.logger.debug "Mikyung > next step will be #{step}"
-      current = try_to_execute(step.new, current, 3)
+      step = step.new if step.kind_of? Class
+      current = try_to_execute(step, current, 3)
     end
     
   end
